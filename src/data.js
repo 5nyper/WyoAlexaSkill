70f7cdc52d2d9c672cc064ae3f99cc0b44b29778
Sinatra <root@stackin.money>
2016-09-07 10:45:11 -0400

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,10 +1,26 @@
 var request = require('request');
+var file = require('fs');
 
+var tag = "<script type='application/ld+json'>";
 
-var res;
+var date = new Date();
 
-request("http://www.maxpreps.com/high-schools/wyomissing-spartans-(wyomissing,pa)/soccer/schedule.htm", function (error, response, body) {
-  res = body.slice(body.indexOf("<script type='application/ld+json'>"));
-  var okay = res.slice(0, res.indexOf("</script>"));
-  console.log(okay);
-});
+function getBoysSoccer(when) {
+  request("http://www.maxpreps.com/high-schools/wyomissing-spartans-(wyomissing,pa)/football/schedule.htm", function(error, response, body) {
+    var res = body.slice(body.indexOf("<script type='application/ld+json'>") + tag.length);
+    var okay = res.slice(0, res.indexOf("</script>"));
+    var schedule = JSON.parse(okay);
+    for (var i = 0; i<schedule.event.length; i++) {
+      if (date.toISOString() > schedule.event[i].StartDate) {
+        console.log("true");
+        continue;
+      }
+      else {
+        console.log(console.log(schedule.event[i].Description));
+        break;
+      }
+    }
+  })
+}
+
+getBoysFootball();
